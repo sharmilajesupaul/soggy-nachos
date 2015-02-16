433675deb3e4fd68d591984ea153d7715dfdf9ac
Sharmila Jesupaul <tosharmila@gmail.com>
2015-02-15 19:56:49 -0800

    modularizes user login and signup

---
+++ client/public/angular/factories/userFactory.js
@@ -0,0 +1,11 @@
+angular.module('userFactory', [])
+.factory('authFactory', ['$http', function($http){
+  return {
+    signup: function (user) {
+      return $http.post('http://localhost:3000/users', user);
+    },
+    login: function (user) {
+      return $http.post('http://localhost:3000/login', user);
+    }
+  };
+}]);
