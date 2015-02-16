6c4835c62357bfa5915d60295ad49fcbe10fd5e2
Sharmila Jesupaul <tosharmila@gmail.com>
2015-02-15 19:36:22 -0800

    adds local auth, user creation

--- client/public/angular/app.js
+++ client/public/angular/app.js
@@ -1,16 +1,32 @@
 angular.module('soggyNachos', ['firebase'])
 
-.controller('MainCtrl', ['$scope', '$firebase', function($scope, $firebase){
+.controller('MainCtrl', ['$scope', '$firebase', '$http', function($scope, $firebase, $http){
 
   // connect to firebase
   var ref = new Firebase("https://amber-heat-3761.firebaseio.com/chat");
   var fb = $firebase(ref);
   // var syncObject = fb.$asArray();
 
-   $scope.messages = fb.$asArray();
+ $scope.messages = fb.$asArray();
+}])
 
-  // console.log('firebase sync', syncObject);
 
-  $scope.messages.$add({message: 'test'});
+.controller('LoginCtrl', ['$scope', '$http', function($scope, $http){
+  $scope.user = {};
 
-}]);
+// Simple POST request example (passing data) :
+  $scope.createUser = function () {
+    $http.post('http://localhost:3000/users', $scope.user)
+    .success(function(data, status, headers, config) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(data);
+      })
+    .error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(data);
+      });
+  };
+}])
+
--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::API
-	before_filter :cors_set_access_control_headers
 
-	def cors_set_access_control_headers
-	  headers['Access-Control-Allow-Origin'] = '*'
-	  headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-	  headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-	end
 end
