f8edd3854f3e6c5c2f144913bff5708b8c903a15
Sharmila Jesupaul <tosharmila@gmail.com>
2015-02-15 19:35:00 -0800

    adds rack-cors gem

--- server/config/application.rb
+++ server/config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module Server
   class Application < Rails::Application
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 0
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
