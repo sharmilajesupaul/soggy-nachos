25c39c4b01a785767aa973789ac11b063847807e
Sharmila Jesupaul <tosharmila@gmail.com>
2015-02-17 12:08:39 -0800

    adds angular material tabs to landing page

--- client/public/angular/controllers/authController.js
+++ client/public/angular/controllers/authController.js
@@ -2,13 +2,22 @@ angular.module('authController', [])
 .controller('LoginCtrl', ['$scope', '$http', '$localStorage', 'authFactory', function($scope, $http, $localStorage, authFactory){
 
     $scope.tabData = {
-      selectedIndex : 0
+      selectedIndex : 0,
+      signupTabIndex : 0
     };
+
     $scope.nextTab = function() {
       $scope.tabData.selectedIndex = Math.min($scope.tabData.selectedIndex + 1, 2) ;
     };
     $scope.previousTab = function() {
       $scope.tabData.selectedIndex = Math.max($scope.tabData.selectedIndex - 1, 0);
+    };
+
+    $scope.nextInnerTab = function() {
+      $scope.tabData.signupTabIndex = Math.min($scope.tabData.signupTabIndex + 1, 2) ;
+    };
+    $scope.previousInnerTab = function() {
+      $scope.tabData.signupTabIndex = Math.max($scope.tabData.signupTabIndex - 1, 0);
     };
 
   $scope.userData = {};
