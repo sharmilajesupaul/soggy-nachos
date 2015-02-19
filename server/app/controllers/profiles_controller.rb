1fa6b76f4d4164b678f24f52213733829d1fef57
Mark Reid <mark.francis.reid@gmail.com>
2015-02-18 17:03:34 -0800

    adds index, create routes for profile and skills controller

--- server/app/controllers/profiles_controller.rb
+++ server/app/controllers/profiles_controller.rb
@@ -1,2 +1,35 @@
 class ProfilesController < ApplicationController
+	,
+	def index
+		user = User.find(params[:user_id])
+		profile = Profile.find_by(user: user)
+		render json: profile
+	end
+
+	def create
+		user = User.find(params[:user_id])
+		profle = Profile.new(user: user)
+		if profile.save
+			render json: profile
+		else
+			session[:errors] = "invalid profile data"
+		end
+	end
+
+	def edit
+		profile = Profile.find(params[:id])
+	end
+
+	def show
+		profile = Profile.find(params[:id])
+	end
+
+	def update
+		profile = Profile.find(params[:id])
+	end
+
+	def destroy
+		Profile.destroy(params[:id])
+	end
+
 end
--- server/app/controllers/skills_controller.rb
+++ server/app/controllers/skills_controller.rb
@@ -1,2 +1,51 @@
 class SkillsController < ApplicationController
+
+	def index
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		profile_skills = profile.profile_skills
+		skills = profile_skills.map{ |profile_skill| profile_skill.skill }
+		render json: skills
+	end
+
+	def create
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		skills = params[:skills] # receiving array of skill ids from user signup?
+		skills.each do |skill_id|
+			skill.find(skill_id)
+			profile_skill = Profile_Skill.new(profile: profile, skill: skill_id)
+			if profile_skill.save
+				
+			else
+				session[:errors] = "invalid profile data"
+			end
+		end
+	end
+
+	def edit
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		profile_skills = profile.profile_skills
+	end
+
+	def show
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		skill = Skill.find(params[:id])
+	end
+
+	def update
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		skill = Skill.find(params[:id])
+	end
+
+	def destroy
+		user = User.find(params[:user_id])
+		profile = Profile.find(params[:profile_id])
+		skill = Skill.find(params[:id])
+	end
+
+
 end
