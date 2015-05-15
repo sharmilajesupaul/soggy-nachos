59a9675ab93cef7b7584fd77bf13f8b7b774a3c9
Mark Reid <mark.francis.reid@gmail.com>
2015-05-14 19:04:39 -0700

    formats code with jshint

--- client/public/angular/app.js
+++ client/public/angular/app.js
@@ -1,51 +1,52 @@
 angular.module('soggyNachos', ['firebase',
-                               'ui.router',
-                               'ngStorage',
-                               'authController',
-                               'ngMaterial',
-                               'ngMessages',
-                               'ngMdIcons',
-                               'dashController',
-                               'themeConfig',
-                               'userFactory',
-                               'dashFactory',
-                               'userRoutes',
-                               'dashRoutes'])
+  'ui.router',
+  'ngStorage',
+  'authController',
+  'ngMaterial',
+  'ngMessages',
+  'ngMdIcons',
+  'dashController',
+  'themeConfig',
+  'userFactory',
+  'dashFactory',
+  'userRoutes',
+  'dashRoutes'
+])
 
-.run(['$location', '$state', 'authenticationCheck', '$localStorage', function($location, $state, authenticationCheck, $localStorage){
-    // console.log($localStorage.user);
-
-
-    if (authenticationCheck.check()) {
-      $location.path('/dash');
-    } else {
-      $location.path("/");
-    }
+.run(['$location', '$state', 'authenticationCheck', '$localStorage', function($location, $state, authenticationCheck, $localStorage) {
+  // console.log($localStorage.user);
 
 
-    // $location.path("/");
+  if (authenticationCheck.check()) {
+    $location.path('/dash');
+  } else {
+    $location.path("/");
+  }
+
+
+  // $location.path("/");
 }])
 
-.directive('ngEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-
-                event.preventDefault();
-            }
+.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if (event.which === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.ngEnter);
         });
-    };
+
+        event.preventDefault();
+      }
+    });
+  };
 })
 
-.controller('MainCtrl', ['$scope', '$firebase', '$http', function($scope, $firebase, $http){
+.controller('MainCtrl', ['$scope', '$firebase', '$http', function($scope, $firebase, $http) {
 
   // connect to firebase
   var ref = new Firebase("https://amber-heat-3761.firebaseio.com/chat");
   var fb = $firebase(ref);
   // var syncObject = fb.$asArray();
 
- $scope.messages = fb.$asArray();
+  $scope.messages = fb.$asArray();
 }]);
--- client/public/angular/routes/dashRoutes.js
+++ client/public/angular/routes/dashRoutes.js
@@ -1,21 +1,21 @@
 angular.module('dashRoutes', [])
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('mainDash', {
-    url           : '/dash',
-    templateUrl   : "angular/templates/mainDash.html",
-    controller    : 'DashCtrl',
-    abstract      : true
-  })
+      .state('mainDash', {
+      url: '/dash',
+      templateUrl: "angular/templates/mainDash.html",
+      controller: 'DashCtrl',
+      abstract: true
+    })
 
-  .state('mainDash.feed', {
-  	url           : '',
-    templateUrl   : "angular/templates/dashFeed.html",
-    controller    : 'DashCtrl'
-  })
+    .state('mainDash.feed', {
+      url: '',
+      templateUrl: "angular/templates/dashFeed.html",
+      controller: 'DashCtrl'
+    });
 
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-}]);
+  }]);
